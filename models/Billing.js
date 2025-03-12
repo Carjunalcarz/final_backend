59e6776154917f325b06dfdd2cd5af688e7aeaa0
carjunalcarz <cardona.aljun.csucc@gmail.com>
2025-03-12 16:00:36 +0800

    with action check payment delete

--- models/Billing.js
+++ models/Billing.js
@@ -1,19 +1,24 @@
 // models/Billing.js
 import mongoose from 'mongoose';
 
-const billingSchema = new mongoose.Schema({
-  subscription_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Subscription', required: true },
-  servicePlan_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ServicePlan', required: true },
-  amountDue: { type: Number},
-  daysUsed : {type : Number },
-  startDate: { type: Date },
-  dueDate: { type: Date },
-  paidDate: { type: Date },
-  isPaid: { type: Boolean},
-  totalAmount : {type : Number},
-  balance : {type : Number},
-  paymentAmount: {type : Number}
-});
+const billingSchema = new mongoose.Schema(
+  {
+    subscription_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Subscription', required: true },
+    servicePlan_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ServicePlan', required: true },
+    approved_user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false},
+    status : {type : Boolean},
+    amountDue: { type: Number },
+    daysUsed: { type: Number },
+    startDate: { type: Date },
+    dueDate: { type: Date },
+    paidDate: { type: Date },
+    isPaid: { type: Boolean, default: false }, // Default value set to false
+    totalAmount: { type: Number },
+    balance: { type: Number },
+    paymentAmount: { type: Number },
+  },
+  { timestamps: true } // Automatically adds `createdAt` and `updatedAt` fields
+);
 
 const Billing = mongoose.model('Billing', billingSchema);
 
